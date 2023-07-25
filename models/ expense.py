620dbb2a6ed117381483b870df45f762e574f25c
Felipe <f.alfonso@res-ear.ch>
2023-07-25 01:24:27 -0400

    new formula

---
+++ models/ expense.py
@@ -0,0 +1,5 @@
+class Expense:
+    def __init__(self, expense_type, amount, date):
+        self.expense_type = expense_type
+        self.amount = amount
+        self.date = date
--- spendwise_app.py
+++ spendwise_app.py
@@ -1,38 +1,53 @@
 import sys
+#print(sys.path)
+
+import os
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QFormLayout, QGroupBox, QDateEdit
 from PyQt5.QtCore import Qt, QDate
-from datetime import datetime
 
-# Class to represent an expense
-class Expense:
-    def __init__(self, expense_type, amount, date):
-        self.expense_type = expense_type
-        self.amount = amount
-        self.date = date
+# Adjust the Python path to include the parent directory of the models folder
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
 
-# Class for the expense tracking application
-class ExpenseTrackerApp(QMainWindow):
+# Import the Expense class using a relative import
+from models.expense import Expense
+
+
+# Import the Database class from the database.py file
+from database import Database
+
+class SpendWisePy(QMainWindow):
+    # Rest of the code remains the same
+
+
     def __init__(self):
         super().__init__()
 
-        self.monthly_budget = 1000  # Set the initial monthly budget
         self.expenses = []
-
         self.initUI()
 
+        # Initialize the database
+        self.database = Database(host='localhost',   # Replace with your MySQL host
+                                 user='root',   # Replace with your MySQL user
+                                 password='fafarafa',  # Replace with your MySQL password
+                                 database_name='SpendWisePy')  # Replace with your database name
+        self.database.connect()
+
     def initUI(self):
-        self.setWindowTitle('Expense Tracking App')
+        self.setWindowTitle('SpendWisePy - Expense Tracker')
         self.setGeometry(100, 100, 400, 400)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
-        # Input widgets
+        # Widgets for input
         self.expense_type_input = QLineEdit()
         self.amount_input = QLineEdit()
         self.date_input = QDateEdit(QDate.currentDate())
 
-        # Button to add an expense
+        # Button to add expense
         add_button = QPushButton('Add Expense')
         add_button.clicked.connect(self.add_expense)
 
@@ -61,22 +76,23 @@ class ExpenseTrackerApp(QMainWindow):
         expense = Expense(expense_type, amount, date)
         self.expenses.append(expense)
 
+        # Save the expense to the MySQL database
+        self.database.save_expense(expense_type, amount, date)
+
         # Update the summary
         self.show_summary()
 
     def calculate_total_expenses(self):
         return sum(expense.amount for expense in self.expenses)
 
-    def calculate_remaining_budget(self):
-        return self.monthly_budget - self.calculate_total_expenses()
-
     def show_summary(self):
         summary_text = "----- Expense Summary -----\n"
         for expense in self.expenses:
             summary_text += f"{expense.expense_type}: {expense.amount} - Date: {expense.date}\n"
-        summary_text += f"Total Expenses: {self.calculate_total_expenses()}\n"
-        summary_text += f"Remaining Budget: {self.calculate_remaining_budget()}"
+        summary_text += f"Total Expenses: {self.calculate_total_expenses()}"
         self.summary_label.setText(summary_text)
+
+# Rest of the code remains the same
 
 # Function to show an error message
 def show_error_message(message):
@@ -88,7 +104,7 @@ def show_error_message(message):
 # Main function
 def main():
     app = QApplication(sys.argv)
-    window = ExpenseTrackerApp()
+    window = SpendWisePy()
     window.show()
     sys.exit(app.exec_())
 
